cb091c2510c6f94637d880f73815fd4f9ada9ba4
Stella-Tahmazyan <stella02051994@gmail.comn>
2024-04-05 12:40:33 +0400

    mastre branch first

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-	//Barev
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
